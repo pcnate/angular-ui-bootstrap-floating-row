4ca177c1134cff7f835cd1a1dae69286cbf37e10
Nathan Baker <pcnate@gmail.com>
2015-06-14 14:48:57 -0500

    tidy up a bit

--- floatingRow/floatingRow.js
+++ floatingRow/floatingRow.js
@@ -1,6 +1,19 @@
+/*
+##################################################################################################
+##                                                                                              ##
+##  Script:   /example/controller.js                                                            ##
+##  Author:   Nathan Baker                                                                      ##
+##  Date:     June 14th, 2015                                                                   ##
+##  Descr:    Basic controller for example usage                                                ##
+##                                                                                              ##
+##################################################################################################
+*/
+
+angular.
+
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 // directive to allow item to scroll with top of page or stay where it belongs
-  .directive( 'floatingAlert', function( $document, $window ) {
+  .directive( 'floatingRow', function( $document, $window ) {
     return {
       restrict: 'A',
       link: function($scope, $element) {
@@ -12,8 +25,7 @@
           var elemTop = $element.parent().parent().offset().top;
           var elemBottom = elemTop + $element.parent().parent().height();
 
-          if((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= 
-docViewBottom) &&  (elemTop >= docViewTop) ) {
+          if((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) ) {
             // console.log( 'visible' );
             $element.parent().parent().css("height", "auto" );
             $element
@@ -21,8 +33,7 @@ docViewBottom) &&  (elemTop >= docViewTop) ) {
               .css("z-index",  "auto" )
               .css("width", $element.parent().parent().css("width") )
               .css("top", $element.parent().parent().css("top") );
-            // $element.css("position", "absolute").css("width", $element.css("width") 
-).css("top", $element.css("top") );
+            // $element.css("position", "absolute").css("width", $element.css("width") ).css("top", $element.css("top") );
           } else {
             // console.log( 'invisible' );
             $element.parent().parent().css("height", $element.css("height") );
@@ -31,8 +42,7 @@ docViewBottom) &&  (elemTop >= docViewTop) ) {
               .css("z-index",  "1000" )
               .css("top", "0px")
               .css("width", $element.parent().parent().css("width") );
-            // $element.css("position", "fixed").css("top", "0px").css("width", 
-$element.css("width") );
+            // $element.css("position", "fixed").css("top", "0px").css("width", $element.css("width") );
           }
         });
       }
